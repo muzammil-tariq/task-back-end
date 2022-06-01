7eee8b8d0928e668d0efb8e206c89aee257cf8a5
--local <atif.sik252@gmail.com>
2022-06-01 16:55:19 +0500

    Event: Added Sort and filter by status & type params

--- src/modules/events/actions/event.get.action.js
+++ src/modules/events/actions/event.get.action.js
@@ -1,16 +1,28 @@
 exports.get = {
   list: async (req, res, next) => {
     try {
-      const data = await models.Events.find({ isDeleted: false }).populate({
-        path: "subCategories",
-        populate: {
-          path: "category",
-        },
-      });
+      const {
+        query: { status, type, sort = 1 },
+      } = req;
+      const where = { isDeleted: false };
+      if (status) {
+        where["status"] = status;
+      }
+      if (type) {
+        where["type"] = type;
+      }
+      const data = await models.Events.find(where)
+        .sort({ createdAt: sort })
+        .populate({
+          path: "subCategories",
+          populate: {
+            path: "category",
+          },
+        });
       return res.json({
         status: 200,
         message: messages.success,
-        data: data,
+        data,
       });
     } catch (error) {
       next(error);
