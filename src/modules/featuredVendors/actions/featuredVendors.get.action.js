fd88ca3b1676c6f899f7ba53ae643aa9f7a12edf
RanaWaqas <rana.waqas@frizhub.com>
2022-06-09 14:36:17 +0500

    public vendor api done

--- src/modules/featuredVendors/actions/featuredVendors.get.action.js
+++ src/modules/featuredVendors/actions/featuredVendors.get.action.js
@@ -1,7 +1,12 @@
 exports.get = {
   getList: async (req, res, next) => {
     try {
-      const featuredVendors = await models.Vendors.find({ featured: true });
+      const featuredVendors = await models.Vendors.find({
+        featured: true,
+      }).populate({
+        path: "skills",
+        populate: { path: "category", select: "category" },
+      });
       return res.status(200).json({
         status: 200,
         message: messages.success,
