1f6bcc2f37bd9fffd4ba26c8368b2e1dbe2ce195
--local <atif.sik252@gmail.com>
2022-06-14 16:55:51 +0500

    Quote: Only allow to quote if event lies in the range

--- src/modules/quotes/actions/quote.add.action.js
+++ src/modules/quotes/actions/quote.add.action.js
@@ -1,20 +1,20 @@
 const QuoteCrudService = new services.CrudService(models.Quotes);
+const { EVENT_REQUEST_DISTANCE } = constants;
+
 exports.add = {
   quote: async (req, res, next) => {
     try {
       const {
-        user: { _id: vendorId, paypalMerchantId },
+        user: {
+          _id: vendorId,
+          paypalMerchantId,
+          location: { coordinates },
+        },
         params: { id: eventId },
         body: payload,
       } = req;
       if (!paypalMerchantId) {
         throw createError(400, messages.paypalNotConnected);
-      }
-      const event = await models.Events.findOne({
-        _id: mongoose.Types.ObjectId(eventId),
-      });
-      if (!event) {
-        throw createError(400, messages.notFound("Event"));
       }
       const alreadyExists = await models.Quotes.findOne({
         eventId: mongoose.Types.ObjectId(eventId),
@@ -23,6 +23,22 @@ exports.add = {
       if (alreadyExists) {
         throw createError(409, messages.alreadyExists("Quote"));
       }
+      const event = await models.Events.findOne({
+        _id: eventId,
+        location: {
+          $near: {
+            $geometry: {
+              type: "Point",
+              coordinates,
+            },
+            $maxDistance: EVENT_REQUEST_DISTANCE,
+          },
+        },
+      });
+      if (!event) {
+        throw createError(400, messages.notFound("Event"));
+      }
+
       const quote = await QuoteCrudService.add({
         ...payload,
         eventId,
