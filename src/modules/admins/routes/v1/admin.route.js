8771d6c96c06c2510dc036ca51664a3e9c682cb3
--local <atif.sik252@gmail.com>
2022-06-14 16:42:17 +0500

    Admin: get stats in a range

--- src/modules/admins/routes/v1/admin.route.js
+++ src/modules/admins/routes/v1/admin.route.js
@@ -1,10 +1,17 @@
-router.post(
-  "/admins/auth/login",
-  validators.admins.signInPayloadValidation,
-  middlewares.validation.request,
-  middlewares.local_passport.authenticate,
-  actions.admins.auth.signIn
-);
+const { USER_ROLE } = constants;
+router
+  .post(
+    "/admins/auth/login",
+    validators.admins.signInPayloadValidation,
+    middlewares.validation.request,
+    middlewares.local_passport.authenticate,
+    actions.admins.auth.signIn
+  )
+  .get(
+    "/admins/stats",
+    middlewares.verifyUserRole(USER_ROLE.ADMIN),
+    actions.admins.get.stats
+  );
 router.post("/admins/seeder", actions.admins.auth.seeder);
 
 module.exports = { prefix: "admins", router };
