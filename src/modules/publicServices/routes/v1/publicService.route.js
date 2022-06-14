995aa341ef6935a4772866b35d181829109812dc
--local <atif.sik252@gmail.com>
2022-06-14 18:05:15 +0500

    Services: Deleted services APIs

--- src/modules/publicServices/routes/v1/publicService.route.js
+++
@@ -1,3 +0,0 @@
-router.get("/services/public", actions.publicServices.get.getList);
-router.get("/services/public/:id", actions.publicServices.get.getById);
-module.exports = { prefix: "services", router };
