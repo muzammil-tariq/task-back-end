c2d90d9da8d320bc86a313ab0bf3615b6f1b4068
--local <atif.sik252@gmail.com>
2022-06-14 17:09:42 +0500

    Category: added sort, filter & pagination

--- src/modules/publicServices/actions/publicService.get.action.js
+++ src/modules/publicServices/actions/publicService.get.action.js
@@ -1,7 +1,23 @@
 exports.get = {
   getList: async (req, res, next) => {
     try {
-      const data = await models.EventSubCategories.find({});
+      const {
+        query: {
+          zipCode,
+          text = "",
+          limit = dataConstraint.PAGINATION_LIMIT,
+          currentPage = dataConstraint.CURRENT_PAGE,
+          sortBy = "createdAt",
+          sortDirection = -1,
+        },
+      } = req;
+      const where = {};
+      if (zipCode) where["zipCode"] = zipCode;
+      if (text) where["name"] = { $regex: text, $options: "i" };
+      const data = await models.EventSubCategories.find(where)
+        .skip(limit * currentPage - limit)
+        .limit(limit)
+        .sort({ [sortBy]: sortDirection });
       res.status(200).json({ msg: "Service List", data });
     } catch (err) {
       next(err);
