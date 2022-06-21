b9b93ab87479f16a5ea9de1f7ef766fa2a9e24de
--local <atif.sik252@gmail.com>
2022-06-21 15:28:56 +0500

    Customer: updated location validator

--- src/modules/customers/customer.validator.js
+++ src/modules/customers/customer.validator.js
@@ -51,26 +51,52 @@ let signUpPayloadValidation = [
     .withMessage(messages.notEmpty)
     .isString()
     .withMessage(messages.invalidDataType("String")),
-  body("location")
+  body("location").notEmpty().withMessage(messages.notEmpty),
+  body("location.coordinates")
+    .exists()
+    .withMessage(messages.notPresent)
     .notEmpty()
     .withMessage(messages.notEmpty)
+    .isArray()
+    .withMessage(messages.invalidDataType("Array"))
     .custom((value) => {
-      if (!value?.coordinates?.length) {
+      if (!value?.length) {
         throw createError(
           400,
           messages.invalidDataType("location.coordinates")
         );
       }
-      if (value?.coordinates?.length !== 2) {
+      if (value?.length !== 2) {
         throw createError(400, messages.invalidFormat("coordinates"));
       }
-      value?.coordinates.forEach((item) => {
+      value.forEach((item) => {
         if (typeof item !== "number") {
           throw createError(400, messages.invalidFormat("coordinates"));
         }
       });
       return true;
     }),
+  body("location.address")
+    .exists()
+    .withMessage(messages.notPresent)
+    .notEmpty()
+    .withMessage(messages.notEmpty)
+    .isString()
+    .withMessage(messages.invalidDataType("String")),
+  body("location.state")
+    .exists()
+    .withMessage(messages.notPresent)
+    .notEmpty()
+    .withMessage(messages.notEmpty)
+    .isString()
+    .withMessage(messages.invalidDataType("String")),
+  body("location.country")
+    .exists()
+    .withMessage(messages.notPresent)
+    .notEmpty()
+    .withMessage(messages.notEmpty)
+    .isString()
+    .withMessage(messages.invalidDataType("String")),
 ];
 
 let signInPayloadValidation = [
