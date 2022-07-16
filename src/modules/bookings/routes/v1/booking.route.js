77c53c183d8be61fc867216eab0827eb2329d1d3
Atif <atif.sik252@gmail.com>
2022-07-16 16:43:08 +0500

    Booking: added customer role guard on create

--- src/modules/bookings/routes/v1/booking.route.js
+++ src/modules/bookings/routes/v1/booking.route.js
@@ -2,6 +2,7 @@ const { USER_ROLE } = constants;
 router
   .post(
     "/bookings",
+    middlewares.verifyUserRole(USER_ROLE.CUSTOMER),
     middlewares.removeNullishValuesFromBody,
     validators.bookings.add,
     middlewares.validation.request,
