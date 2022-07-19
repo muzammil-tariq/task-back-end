b6ad9e22510e9fd7796856ebc82dafb10811c2f2
hassann691 <hassan.mehboob@frizhub.com>
2022-07-19 12:53:27 +0500

    userId: adding userId in newsLetter schema

--- src/modules/newsLetter/newsLetter.schema.js
+++ src/modules/newsLetter/newsLetter.schema.js
@@ -1,4 +1,8 @@
 const NewsLetterSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
+  userId:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+  }
 });
 module.exports = mongoose.model("NewsLetter", NewsLetterSchema);
