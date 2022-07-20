bba037fdce43eae439bb7366296caf51c6b17e77
Atif <atif.sik252@gmail.com>
2022-07-20 17:23:41 +0500

    Renamed file

--- src/middlewares/jwt.middleware.js
+++ src/middlewares/jwt.middleware.js
@@ -4,7 +4,7 @@ const roleModel = {
   vendors: models[USER_ROLE.VENDOR],
   admins: models[USER_ROLE.ADMIN],
 };
-router.use(function (req, res, next) {
+router.use(function jwtMiddleware(req, res, next) {
   let isChecked = 0;
   for (let route in roleModel) {
     if (req.path.search(`/${route}`) != -1 && !isChecked) {
@@ -15,7 +15,7 @@ router.use(function (req, res, next) {
   passport.authenticate("jwt", { session: false }, function (err, user, info) {
     if (err || !user) {
       if (
-        utils.isObjInArray(constants.PUBLIC_ROUTES, {
+        utils.isPublicRequest({
           method: req.method,
           path: req.path,
         })
